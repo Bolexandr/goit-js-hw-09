d1b4bd031df35e2c64a89ec82312f9a1ecf2b1d9
Olexandr Beisiuk <57046480+Bolexandr@users.noreply.github.com>
2022-09-09 15:41:05 +0300

    ДЗ 2 Ок

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,92 @@
 const bodyLinc = document.querySelector('body');
+const btn = bodyLinc.querySelectorAll('button');
 
-bbody.style.bacground
+
+// функція по зміні backgroundColor  приймає посилання на 
+// потрібний елемент і приймає колір у виді hex , 
+// або функції по зміні кольору)
+function changerBGColor(elemLinc, color) {
+  if (`${typeof color}` === 'function' ) {
+    setColor = color()
+    console
+  }else{
+  setColor = color
+  }
+  elemLinc.style.backgroundColor = setColor
+  // console.log('changerBGColor',setColor)
+}
+
+ let bull = confirm('натисни <<ок>> якщо хочеш побачити завдання виконане згідно ТЗ до домашки , Але якщо натиснеш <<скасувати>> тоді ти пізнаєш блаженство дискополо' )
+
+////Функція рандомного кольору
+function randomColorInHex() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  return "#" + randomColor;
+}
+
+bodyLinc.addEventListener('click', eventHendler)
+
+document.querySelector('[data-stop]').disabled = true
+  
+////Повністю автономна колбек функція
+function eventHendler(e) {
+  if (bull) {
+    btn[0].classList.remove('discoStyleBtn')
+    btn[1].classList.remove('discoStyleBtn')
+   if (e.target.textContent === 'Start') {
+    clearInterval(sessionStorage.getItem('timerId'));
+    changerBGColor(bodyLinc, randomColorInHex)
+    timerId = setInterval(changerBGColor, 1000, bodyLinc, randomColorInHex)
+    sessionStorage.setItem('timerId', timerId)
+    // console.log('event Start',timerId)
+        document.querySelector('[data-stop]').disabled=false
+
+    e.target.disabled=true
+    // console.dir(e)
+    
+  }
+  else if (e.target.textContent === 'Stop') {
+    timerId = sessionStorage.getItem('timerId')
+    clearInterval(timerId);
+    // console.log('event Stop',timerId);
+     e.target.disabled = true;
+     document.querySelector('[data-start]').disabled = false;
+    // console.log( document.querySelector('[data-start]'))
+      bull = confirm('натисни <<ок>> якщо хочеш побачити завдання виконане згідно ТЗ до домашки , Але якщо натиснеш <<скасувати>> тоді ти пізнаєш блаженство дискополо' )
+  }
+   
+  } else {
+
+    btn[0].classList.add('discoStyleBtn')
+    btn[1].classList.add('discoStyleBtn')
+
+   if (e.target.textContent === 'Start') {
+    clearInterval(sessionStorage.getItem('timerId'));
+    changerBGColor(bodyLinc, randomColorInHex)
+    timerId = setInterval(changerBGColor, 300, bodyLinc, randomColorInHex)
+    sessionStorage.setItem('timerId', timerId)
+    // console.log('event Start',timerId)
+        document.querySelector('[data-stop]').disabled=false
+
+    e.target.disabled=true
+    // console.dir(e)
+    
+  }
+  else if (e.target.textContent === 'Stop') {
+    timerId = sessionStorage.getItem('timerId')
+    clearInterval(timerId);
+    // console.log('event Stop',timerId);
+     e.target.disabled=true
+    document.querySelector('[data-start]').disabled=false
+    // console.log( document.querySelector('[data-start]'))
+    bull = confirm('натисни <<ок>> якщо хочеш побачити завдання виконане згідно ТЗ до домашки , Але якщо натиснеш <<скасувати>> тоді ти пізнаєш блаженство дискополо' )
+
+  }
+  
+}
+
+
+
+
+
+// console.dir(typeof randomColorInHex)
---
+++ src/js/02-timer.js
@@ -0,0 +1,93 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const btn = document.querySelector('button');
+// btn.disabled=true
+const inpt = document.querySelector('#datetime-picker');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    console.log(selectedDates[0].getTime());
+    console.log(new Date().getTime());
+    if (new Date().getTime() < selectedDates[0].getTime()) {
+      btn.disabled = false;
+      Notiflix.Notify.success('You choose a date in the future , great!!!');
+    } else {
+      btn.disabled = true;
+      Notiflix.Notify.failure('Please choose a date in the future');
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+let intervalId = null;
+
+function diffTime(finishDate) {
+  ms = new Date(finishDate).getTime() - new Date().getTime();
+
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  // console.log({ days, hours, minutes, seconds });
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value, countZero) {
+  if (value <= 99) {
+    return value.toString().padStart(2, 0);
+  } else {
+    return value.toString().padStart(countZero, 0);
+  }
+}
+
+function changerValueOnPage(inptValue) {
+  const obj= diffTime(inptValue)
+  const vallArr = Object.values(obj).reduce( (acumulator, item) => acumulator + item ,0)
+
+  if ( vallArr >= 0 ){
+    console.log('sumAllNumbersOfDate=',vallArr)
+
+     const fatherDiv = document.querySelector('.timer');
+
+  ///////Перший спосіб:
+  // const { days, hours, minutes, seconds } = obj;
+  // fatherDiv.querySelector('[data-days]').textContent = addLeadingZero(days, 3);
+  // fatherDiv.querySelector('[data-hours]').textContent = addLeadingZero( hours,2);
+  // fatherDiv.querySelector('[data-minutes]').textContent = addLeadingZero(minutes,2);
+  // fatherDiv.querySelector('[data-seconds]').textContent = addLeadingZero(seconds,2);
+
+  //////Другий спосіб:
+  for (let ell of Object.keys(obj)) {
+    fatherDiv.querySelector(`[data-${ell}]`).textContent = addLeadingZero(obj[ell], 3 );
+  }
+  }else{
+    console.log('Event: (clearInterval) = true')
+    clearInterval(intervalId)
+  }
+ 
+}
+
+function hednlerEvent(e) {
+  clearInterval(intervalId)
+  console.log('Event: (setInterval) = true')
+  intervalId = setInterval(changerValueOnPage ,1000,inpt.value);
+}
+ 
+btn.addEventListener('click', hednlerEvent);
